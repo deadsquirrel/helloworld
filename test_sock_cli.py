a1a5cdf118cbe1be017a4c9c1dcd465ac2e8546f
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-05-18 23:17:48 +0300

    enter IP

--- test_sock_cli.py
+++ test_sock_cli.py
@@ -4,7 +4,9 @@
 import socket
 
 sock = socket.socket()
-sock.connect(('localhost', 9090))
+chost = raw_input('Enter hostname for connections: ')
+sock.connect((chost, 9090))
+#sock.connect(('52.38.123.102', 9090))
 sock.send('hello, world!')
 
 data = sock.recv(1024)
