f14b4c787bb3e255981d6fa2c8a147f3e8941fd1
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-05-18 13:01:07 +0300

    simple server-client socket work

---
+++ test_sock_ser.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import socket
+
+sock = socket.socket()
+
+# host (all), port
+sock.bind(('', 9090))
+
+# мах кол-во подключений в очереди
+sock.listen(1)
+print 'listening... . . .  .   .    .     .'
+
+conn, addr = sock.accept()
+
+print 'conected:', addr
+
+# получаем данные кусками по 1024б
+# и возвращаем их клиенту в вехнем регистре
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
+
+
+conn.close()
