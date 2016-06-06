3072215d1cda08d81caf601b392f4095b25652fe
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-06 23:56:36 +0300

    for django blogs

---
+++ ex_installed_apps.py
@@ -0,0 +1,17 @@
+#settings.py
+#http://podavan.ru/2015/07/25/znakomstvo-s-django-sozdanie-bloga/
+
+
+INSTALLED_APPS = (
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.sites',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+# Uncomment the next line to enable the admin:
+'django.contrib.admin',
+# Uncomment the next line to enable admin documentation:
+# 'django.contrib.admindocs',
+‘blog’,
+)
