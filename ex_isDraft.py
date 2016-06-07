5f775e6af7f43e00746a14f70b1f40a472a61af0
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-07 23:49:28 +0300

    littlefix - def only in class

--- ex_isDraft.py
+++ ex_isDraft.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from testapp.models import Post
      
-def make_published(modeladmin, request, queryset):
-    """ Make all posts published. """
-     queryset.update(is_draft=False)
-     make_published.short_description = "Mark selected posts as published"
-     
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_draft')
     actions = [make_published]
-   
+
+    def make_published(modeladmin, request, queryset):
+        """ Make all posts published. """
+        queryset.update(is_draft=False)
+        make_published.short_description = "Mark selected posts as published"
+
+    
 admin.site.register(Post, PostAdmin)
+
