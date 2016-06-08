e797f926db702a3bbf6c81a5d8c2cc1835842af7
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-08 23:46:17 +0300

    include additional URL patterns by using a list of url()

---
+++ ex_.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+
+from apps.main import views as main_views
+from credit import views as credit_views
+
+extra_patterns = [
+    url(r'^reports/$', credit_views.report),
+    url(r'^reports/(?P<id>[0-9]+)/$', credit_views.report),
+    url(r'^charge/$', credit_views.charge),
+]
+
+urlpatterns = [
+    url(r'^$', main_views.homepage),
+    url(r'^help/', include('apps.help.urls')),
+    url(r'^credit/', include(extra_patterns)),
+]
