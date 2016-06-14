dccb23c576ef5a85cccd59b151f6d46036ef242d
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-14 22:06:54 +0300

    transparent square and blue circle on square

--- trygame/screen.py
+++ trygame/screen.py
@@ -8,13 +8,17 @@ window = pygame.display.set_mode((400, 400))
 
 pygame.display.set_caption('ура! это заголовок окна!')
 
+# запишем цвет в RGB
+BLUE =  (  0,   0, 255)
+
 #игровой экран
 screen = pygame.Surface((400, 400))
 
 # создаем обьект
 square = pygame.Surface((40, 40))
 # красим обьект , цвета в RGB
-square.fill((0, 255, 0))
+square.fill((50, 16, 85))
+
 x = 0
 y = 0
 square_go_right = True
@@ -40,15 +44,17 @@ while done:
             square_go_right = True
 
     if square_go_down == True:
-        y += 1
+        y += 2
         if y > 360: # высота экрана минус ширина обьекта
             square_go_down = False
     else:
-        y -= 5
+        y -= 3
         if y < 0:
             square_go_down = True
     
     # указываем что и куда отображаем
+       # рисуем-создаем  круг на квадрате
+    pygame.draw.circle(square, BLUE, (20, 20), 20, 0)
     screen.blit(square, (x, y))
     window.blit(screen,  (0, 0))
     pygame.display.flip()
