fd65ce69714457578266297968f274247be4e01c
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-14 21:13:56 +0300

    create object on screen

--- trygame/screen.py
+++ trygame/screen.py
@@ -11,6 +11,11 @@ pygame.display.set_caption('ура! это заголовок окна!')
 #игровой экран
 screen = pygame.Surface((400, 400))
 
+# создаем обьект
+square = pygame.Surface((40, 40))
+square.fill((0, 255, 0))
+
+
 done = True
 while done:
     for e in pygame.event.get():
@@ -20,6 +25,8 @@ while done:
 
     #красим окно , цвета в RGB
     screen.fill((50, 16, 85))
-    
+
+    # указываем что и куда отображаем
+    screen.blit(square, (0, 0))
     window.blit(screen,  (0, 0))
     pygame.display.flip()
