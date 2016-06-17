f610d038d2d936b86d7214e6124476786805f081
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-17 22:47:21 +0300

    error at axis Y - screen not ended

--- trygame/game1.py
+++ trygame/game1.py
@@ -24,8 +24,14 @@ class Sprite:
 
 # создаем героев
 boy1 = Sprite(0,0, 'face1.png')
+# новый атрибут герою - рычаг управления_1
+boy1_go_right = True
+boy1_go_down = True
+
 boy2 = Sprite(40,40, 'face2.png')
-        
+# новый атрибут герою - рычаг управления_2
+boy2_go_right = True
+boy2_go_down = True
 
 done = True
 while done:
@@ -33,14 +39,55 @@ while done:
         if e.type == pygame. QUIT:
             done = False
 
-    # красим окно , цвета в RGB
+# красим окно , цвета в RGB
     screen.fill((50, 16, 85))
 
-# отобразим обьекты
+# учим обьекты двигаться
+# boy1
+    if boy1_go_right == True:
+        boy1.x += 1
+        if boy1.x > 360: # ширина экрана минус ширина обьекта
+            boy1_go_right = False
+    else:
+        boy1.x -= 1
+        if boy1.x < 0:
+            boy1_go_right = True
+
+    if boy1_go_down == True:
+        boy1.y += 1
+        if boy1.y > 360: # высота экрана минус ширина обьекта
+            boy1_go_down = False
+    else:
+        boy1.y -= 1
+        if boy1.y < 0:
+            boy1_go_down = True
+
+    # boy2
+    
+    if boy2_go_right == True:
+        boy2.x += 1
+        if boy2.x > 360: # ширина экрана минус ширина обьекта
+            boy2_go_right = False
+    else:
+        boy2.x -= 1
+        if boy2.x < 0:
+            boy2_go_right = True
+
+    if boy2_go_down == True:
+        boy2.y += 1
+        if boy2.y > 360: # высота экрана минус ширина обьекта
+            boy2_go_down = False
+    else:
+        boy2.y -= 1
+        if boy2.y < 0:
+            boy1_go_down = True
+    
+            
+    # отобразим обьекты
     boy1.render()
     boy2.render()
 
     window.blit(screen,  (0, 0))
     pygame.display.flip()
     # делаем задержку
-    pygame.time.delay(5)
+    pygame.time.delay(15)
