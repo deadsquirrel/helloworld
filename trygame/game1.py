9067b34be3b06ab0532bc2c3afb2d8ecba58a65f
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-17 22:21:41 +0300

    add class Sprite , 2 object

--- trygame/game1.py
+++ trygame/game1.py
@@ -10,6 +10,23 @@ pygame.display.set_caption('Game!')
 #игровой экран
 screen = pygame.Surface((400, 400))
 
+#вводим класс для героя
+class Sprite:
+    def __init__ (self,xpos,ypos,filename):
+        self.x=xpos
+        self.y=ypos
+        self.bitmap=pygame.image.load(filename)
+# фон делаем прозрачным
+        self.bitmap.set_colorkey((255,255,255))
+# отображаем обьект на игровой экран
+    def render(self):
+        screen.blit(self.bitmap,(self.x,self.y))
+
+# создаем героев
+boy1 = Sprite(0,0, 'face1.png')
+boy2 = Sprite(40,40, 'face2.png')
+        
+
 done = True
 while done:
     for e in pygame.event.get():
@@ -18,6 +35,10 @@ while done:
 
     # красим окно , цвета в RGB
     screen.fill((50, 16, 85))
+
+# отобразим обьекты
+    boy1.render()
+    boy2.render()
 
     window.blit(screen,  (0, 0))
     pygame.display.flip()
