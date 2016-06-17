ac4eedc4f1b8263e983de117c278af9803cf49e2
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-17 21:49:28 +0300

    create new game

---
+++ trygame/game1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame
+
+# создадим окно (высота, ширина)
+window = pygame.display.set_mode((400, 400))
+pygame.display.set_caption('Game!')
+
+#игровой экран
+screen = pygame.Surface((400, 400))
+
+done = True
+while done:
+    for e in pygame.event.get():
+        if e.type == pygame. QUIT:
+            done = False
+
+    # красим окно , цвета в RGB
+    screen.fill((50, 16, 85))
+
+    window.blit(screen,  (0, 0))
+    pygame.display.flip()
+    # делаем задержку
+    pygame.time.delay(5)
