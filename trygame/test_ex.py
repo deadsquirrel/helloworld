c253ecca08fa6a4b228446451e4811e503a05724
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-20 23:17:00 +0300

    keyboard control

--- trygame/test_ex.py
+++ trygame/test_ex.py
@@ -27,47 +27,91 @@ zet = Sprite(200,0, 'apple.jpeg')
 # новый атрибут герою - рычаг управления_1
 zet_go_right = True
 zet_go = True
+zet.step = 1
 
 hero= Sprite(200,360, 'luchnik.jpg')
 # новый атрибут герою - рычаг управления_2
 hero_go_right = True
 hero_go = True
 
-strela= Sprite(220,360, 'strela.png')
+strela = Sprite(-10,350, 'strela.png')
+strela_go = False
 
 # функция проверки пересечения объектов. 40 - величина ширины объектов
-def Intersect(x1, x2, y1, y2):
-    if ((x1 > x2-40) and (x1 < x2+40) and (y1 > y2-40) and (y1 < y2+40)) :
+def Intersect(x1, x2, y1, y2, db1, db2):
+    if ((x1 > x2-db1) and (x1 < x2+db2) and (y1 > y2-db1) and (y1 < y2+db2)) :
         return 1
     else:
         return 0
                 
 done = True
+# дублируем клавишу, пока оно нажата pygame.key.set_repeat (a, b)
+# a - задержка перед первым дублированием, мс;  b - задержка между остальными дублированиями
+pygame.key.set_repeat (1, 1)
+
 while done:
+    # обработчик событий
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             done = False
            
-# красим окно , цвета в RGB
+    # красим окно , цвета в RGB
     screen.fill((255, 255, 8))
-    
 
-    '''
-    if Intersect (zet.x, hero.x, zet.y, hero.y) == 1:
-        if zet_go == True:
+    # движение цели
+    if zet_go == True:
+        zet.x += zet.step
+        if zet.x > 360: 
             zet_go = False
-        else:
+    else:
+        zet.x -= zet.step
+        if zet.x < 0:
             zet_go = True
-        if hero_go == True:
-            hero_go = False
-        else:
-            hero_go = True
-    '''   
+
+    # событие - нажатие клавиш
+    if e.type == pygame.KEYDOWN:
+        # перемещение героя
+        if e.key == pygame.K_LEFT:
+            if hero.x > 10:
+                hero.x -= 1
+        if e.key == pygame.K_RIGHT:
+            if hero.x < 350:
+                hero.x += 1
+        if e.key == pygame.K_UP:
+            if hero.y > 70:
+                hero.y -= 1
+        if e.key == pygame.K_DOWN:
+            if hero.y < 350:
+                hero.y += 1
+        # запуск стрелы
+        if e.key == pygame.K_SPACE:
+            if strela_go == False:
+                strela.x = hero.x + 15
+                strela.y = hero.y - 15
+                strela_go = True
+
+    # перемещение стрелы
+    if strela.y < 0:
+        strela_go = False
+        
+    if strela_go == False:
+        strela.y = 350
+        strela.x = -10
+    else:
+        strela.y -= 1
+  
+
+    # столкновение стрелы и цели
+    if Intersect (strela.x, zet.x, strela.y, zet.y, 10, 40) == 1:
+        strela_go = False
+        print ("Yes!")
+        zet.step += 0.2
+          
 # отобразим обьекты
+    strela.render()
     zet.render()
     hero.render()
-    strela.render()
-    
+        
     window.blit(screen,  (0, 0))
     pygame.display.flip()
     # делаем задержку
